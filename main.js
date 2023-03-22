fc94f5efe9fe742b065c9b7b1b16ccce8a95484b
DudnikVadim <dudnikvadim9@gmail.com>
2023-03-22 19:02:08 +0500

    Added apple

--- main.js
+++ main.js
@@ -7,9 +7,17 @@ const retryBtn = document.querySelector("#retry-btn")
 const canvas = document.querySelector("#canvas")
 const context = canvas.getContext('2d')
 
+const canvasSize = {
+  width: 400,
+  height: 400,
+}
+const fieldWidth = canvasSize.width / 8
+
 const startGame = () => {
   startMenu.classList.add("hidden")
   canvas.classList.remove("hidden")
+  canvas.width = canvasSize.width
+  canvas.height = canvasSize.height
 
   drawField()
 }
@@ -21,34 +29,51 @@ const endGame = () => {
 }
 
 const drawField = () => {
-  context.rect(0, 0, 400, 400)
+  context.rect(0, 0, canvasSize.width, canvasSize.height)
   context.fill()
 
-  const fieldWidth = 20
-  for(let i = 0; i < 400; i += fieldWidth) {
-    for(let j = 0; j < 400; j += fieldWidth) {
-      context.beginPath()
-      context.lineWidth = 1
-      context.strokeStyle = "red"
-      context.moveTo(i, j)
-      context.lineTo(i + fieldWidth, j)
-      context.lineTo(i + fieldWidth, j + fieldWidth)
-      context.lineTo(i, j + fieldWidth)
-      context.stroke()
-      context.closePath()
+  for(let i = 0; i < canvasSize.width; i += fieldWidth) {
+    for(let j = 0; j < canvasSize.width; j += fieldWidth) {
+      drawCell(i, j, 'red')
     }
   }
 
+  const cellX = rand(1, 8)
+  const cellY = rand(1, 8)
+  drawRect(cellX, cellY, 'green')
+}
+
+const drawCell = (x ,y, color) => {
+  context.beginPath()
+  context.lineWidth = 1
+  context.strokeStyle = color
+  context.moveTo(x, y)
+  context.lineTo(x + fieldWidth, y)
+  context.lineTo(x + fieldWidth, y + fieldWidth)
+  context.lineTo(x, y + fieldWidth)
+  context.stroke()
+  context.closePath()
+}
+
+const drawRect = (x, y, color) => {
+  x = x * fieldWidth
+  y = y * fieldWidth
+  context.beginPath()
+  context.lineWidth = 1
+  context.fillStyle = color
+  context.moveTo(x, y)
+  context.lineTo(x + fieldWidth, y)
+  context.lineTo(x + fieldWidth, y + fieldWidth)
+  context.lineTo(x, y + fieldWidth)
+  context.fill()
+  context.closePath()
+}
+
+const rand = (min, max) => {
+  return Math.floor((Math.random() * max - min) + min)
 }
 
 startGame()
-
-// const drawLine = () => {
-//   context.lineWidth = 1
-//   context.strokeStyle = 'black'
-//   context.moveTo(50, 0)
-//   context.lineTo(50, 400)
-// }
 
 startBtn.addEventListener("click", startGame)
 
